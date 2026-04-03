b514e495f7f931e3e21ec0b2ea246feff27cf584
Fadi Y <fadiyoux@gmail.com>
2026-04-02 22:49:32 -0600

    Show First name

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <p>
+            Fadi Yousif
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
